b3692f6e27cf934673d4a484f50813e4fd561640
kuluna <mailkuluna@gmail.com>
2017-04-09 18:14:38 +0900

    fix angular material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -19,6 +20,7 @@ import { ProjectDetailComponent } from './projects/detail/projectdetail.componen
     ProjectDetailComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     HttpModule,
