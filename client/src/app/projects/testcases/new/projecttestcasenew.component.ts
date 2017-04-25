ea36b786b968f451695d98856c9605fdedf0e429
kuluna <mailkuluna@gmail.com>
2017-04-25 16:58:20 +0900

    Add more create

--- client/src/app/projects/testcases/new/projecttestcasenew.component.ts
+++ client/src/app/projects/testcases/new/projecttestcasenew.component.ts
@@ -2,9 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MdSnackBar } from '@angular/material';
 import { Router, ActivatedRoute } from '@angular/router';
-import { ProjectsClient, Testcase, TestcaseCreateRequest } from '../../../apiclient.service';
-
 import { BodyContentComponent } from '../../../bodycontent.component';
+import { ProjectsClient, Testcase, TestcaseCreateRequest } from '../../../apiclient.service';
 
 @Component({
   selector: 'app-projecttestcasenew',
@@ -13,9 +12,8 @@ import { BodyContentComponent } from '../../../bodycontent.component';
   providers: [ProjectsClient]
 })
 export class ProjectTestcaseNewComponent implements OnInit {
-  submitting: boolean;
-
-  alwaysOk = false;
+  submitting = false;
+  moreCreate = false;
 
   constructor(private router: Router,
               private activeRoute: ActivatedRoute,
@@ -29,18 +27,22 @@ export class ProjectTestcaseNewComponent implements OnInit {
     // fix md-slide-toggle has no default value
     value.moreCareful = value.moreCareful ? value.moreCareful : false;
 
-    console.log(JSON.stringify(value));
     this.submitting = true;
-
     this.activeRoute.params.map(params => params['projectId'] as number)
                            .switchMap(projectId => this.projectsClient.postTestcase(projectId, value))
                            .subscribe(testcase => {
                              this.snackBar.open('Created.', null, { duration: 1500 });
-                             this.router.navigate(['../'], { relativeTo: this.activeRoute });
+                             if (this.moreCreate) {
+                              form.reset();
+                              form.resetForm();
+                              this.submitting = false;
+                             } else {
+                              this.router.navigate(['../'], { relativeTo: this.activeRoute });
+                             }
                            }, error => {
                              this.submitting = false;
-                             console.log(error);
-                             this.snackBar.open(error, null, { duration: 1500 });
+                             console.warn(error);
+                             this.snackBar.open('Error. Try again.', null, { duration: 1500 });
                            });
   }
 }
