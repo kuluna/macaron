f058034310f4b38530a3e186c4879d711ad0782e
kuluna <mailkuluna@gmail.com>
2017-05-29 21:35:12 +0900

    generalize navigation component

--- client/src/app/projectnav.component.ts
+++ client/src/app/projectnav.component.ts
@@ -1,15 +1,20 @@
-import { Component, Directive, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-projectnav',
   template:
     `
     <nav md-tab-nav-bar color="primary">
-      <a md-tab-link [routerLink]="['./']" [active]="true">Overview</a>
-      <a md-tab-link [routerLink]="['testcases']">Testcases</a>
-      <a md-tab-link [routerLink]="['testplans']">Testplans</a>
-      <a md-tab-link [routerLink]="['testruns']">Testruns</a>
+      <a md-tab-link [routerLink]="['/projects', projectId]" [active]="activeTabName == 'overview'">Overview</a>
+      <a md-tab-link [routerLink]="['/projects', projectId, 'testcases']" [active]="activeTabName == 'testcases'">Testcases</a>
+      <a md-tab-link [routerLink]="['/projects', projectId, 'testplans']" [active]="activeTabName == 'testplans'">Testplans</a>
+      <a md-tab-link [routerLink]="['/projects', projectId, 'testruns']" [active]="activeTabName == 'testruns'">Testruns</a>
     </nav>
     `
 })
-export class ProjectNavComponent {}
+export class ProjectNavComponent {
+  @Input()
+  projectId: number;
+  @Input()
+  activeTabName: string;
+}
