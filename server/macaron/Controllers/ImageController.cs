0b9d40efe21a2d2993e8d6cb493792db9e2e3acd
kuluna <mailkuluna@gmail.com>
2017-07-07 12:44:33 +0900

    recieve multipart API

---
+++ server/macaron/Controllers/ImageController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Macaron.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/images")]
+    public class ImageController : Controller
+    {
+
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        public async Task Upload(IFormFile file, string fileName)
+        {
+            if(file.Length > 0)
+            {
+                using (var stream = new FileStream(Path.Combine("assets", "images", fileName), FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+        }
+    }
+}
